3aeb83b6f74257f43526c02cd9aa30168de4534e
Przemyslaw Michalek <pmichalek@apttension.com>
2023-04-03 20:18:08 +0200

    feature: Setup sample server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello world');
+import fastify from 'fastify';
+
+const server = fastify({ logger: true });
+
+server.get('/', async (request, reply) => {
+  return { hello: 'world' };
+});
+
+server.listen({ port: 3000, host: '0.0.0.0' }, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server listening at ${address}`);
+});
